ddfa1ed046f22cd3112b0de4a85877eb7cb01a16
Jaroslav Polakovič <jaroslav.polakovic@xwp.co>
2021-09-04 12:32:17 +0200

    Clean up the Checkout Button block.

--- js/src/editor.js
+++ js/src/editor.js
@@ -1,25 +1,12 @@
 import { registerBlockType } from '@wordpress/blocks';
-import { __ } from '@wordpress/i18n';
-import blockIcon from './components/blockIcon';
 
-// Import JS
-import blockOutput from './block';
+// Import blocks.
+import {
+	name as checkoutButtonName,
+	default as checkoutButton,
+} from './blocks/checkout-button';
 
-export const name = 'fast-co/checkout-button';
+// Import CSS.
+import './editor.scss';
 
-registerBlockType(name, {
-	title: __('Fast.co Block', 'fast-co-block'),
-	icon: blockIcon,
-	keywords: [
-		__('fast', 'fast-co-block'),
-		__('checkout', 'fast-co-block'),
-		__('button', 'fast-co-block'),
-	],
-	category: 'embed',
-	description: __('Fast.co ecommerce block.', 'fast-co-block'),
-	edit: blockOutput,
-
-	save() {
-		return null;
-	},
-});
+registerBlockType(checkoutButtonName, checkoutButton);
